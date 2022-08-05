81de93333ca6acc48e2f5aeb1504db6b49e66eef
Élodie Gagé <elodiegage@gmail.com>
2022-08-05 12:39:59 +0200

    final

---
+++ CompareImage/ComparImage/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ComparImage
+//
+//  Created by Elodie Gage on 26/07/2022.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ CompareImage/ComparImage/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ComparImage
+//
+//  Created by Elodie Gage on 26/07/2022.
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
